cd08fbb7c356f96c8678faacfccf75927a55ed3d
dgmodesto <douglasgomesmodesto@gmail.com>
2021-12-19 22:53:37 -0300

    feat(utopian-tree): implementar solução para o problema utopian tree

---
+++ ChallengeHackerRank/UtopianTree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChallengeHackerRank
+{
+    public static class UtopianTree
+    {
+        /*
+            * Complete the 'utopianTree' function below.
+            * 
+            * The function is expected to return an INTEGER.
+            * The function accepts INTEGER n as parameter.
+            * 
+            * https://www.hackerrank.com/challenges/utopian-tree/problem?isFullScreen=true
+  */
+
+        public static int utopianTree(int n)
+        {
+            int result = 0;
+
+
+
+            return result;
+        }
+
+        public static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                int result = utopianTree(n);
+
+                Console.WriteLine(result);
+            }
+
+        }
+    }
+}
