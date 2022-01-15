54c1a2805667a8eeb5cdd3065c072d87240b8919
dgmodesto <douglasgomesmodesto@gmail.com>
2022-01-15 07:42:00 -0300

    feat(find-digits): implementar solução do problema find digits do hacker hanck

---
+++ ChallengeHackerRank/FindDigits.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChallengeHackerRank
+{
+    public static class FindDigits
+    {
+        /*
+     * Complete the 'findDigits' function below.
+     *
+     *https://www.hackerrank.com/challenges/find-digits/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+        public static int findDigits(int n)
+        {
+            int result = 0;
+
+            var digits = n.ToString().ToCharArray();
+
+            for(int i = 0; i < digits.Length; i++)
+            {
+                var digit = Convert.ToInt64(digits[i].ToString());
+                if ((digit > 0) && (n % digit == 0))
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+
+        public static void Initial(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                int result = findDigits(n);
+
+                Console.WriteLine(result);
+            }
+
+        }
+    }
+}
--- ChallengeHackerRank/Program.cs
+++ ChallengeHackerRank/Program.cs
@@ -12,7 +12,7 @@ namespace ChallengeHackerRank
       
         static void Main(string[] args)
         {
-            JumpingCloudRevisited.Initial(args);
+            FindDigits.Initial(args);
         }
 
     }
