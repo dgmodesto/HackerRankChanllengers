fc5197037e7d810080cce3ccd7613c71a33c9908
dgmodesto <douglasgomesmodesto@gmail.com>
2022-01-22 20:06:18 -0300

    feat(chocolate-feast): implementar solução do problema chocolate feast do hacker hanck

--- ChallengeHackerRank/Program.cs
+++ ChallengeHackerRank/Program.cs
@@ -12,7 +12,7 @@ namespace ChallengeHackerRank
       
         static void Main(string[] args)
         {
-            EqualityInAArray.Initial(args);
+            ChocolateFeast.Initial(args);
         }
 
     }
