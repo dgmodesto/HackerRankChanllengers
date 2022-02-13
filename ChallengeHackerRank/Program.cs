66084c5669a6b42d776d72eec8630c6ea93571da
dgmodesto <douglasgomesmodesto@gmail.com>
2022-02-13 15:54:50 -0300

    feat(acmi-icp-team): implementar solução do problema acmi-icpt-team do hacker hanck

--- ChallengeHackerRank/Program.cs
+++ ChallengeHackerRank/Program.cs
@@ -12,7 +12,7 @@ namespace ChallengeHackerRank
       
         static void Main(string[] args)
         {
-            TaumAndBday.Initial(args);
+            AcmICpcTeam.Initial(args);
         }
 
     }
