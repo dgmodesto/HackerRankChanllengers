a3a8b9a6670e48c5cad81e6dfc81e78ad9495bc9
dgmodesto <douglasgomesmodesto@gmail.com>
2022-04-26 09:55:22 -0300

    feat(find-merge-node): implementar solução do projeto find-merge-node do hacker rank

--- ChallengeHackerRank/Program.cs
+++ ChallengeHackerRank/Program.cs
@@ -12,11 +12,8 @@ namespace ChallengeHackerRank
       
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            KaprekarNumbers.Initial(args);
-=======
-            AcmICpcTeam.Initial(args);
->>>>>>> 66084c5669a6b42d776d72eec8630c6ea93571da
+            FindMergeNode.Initial(args);
+
         }
 
     }
