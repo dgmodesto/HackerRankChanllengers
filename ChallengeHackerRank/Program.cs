1f15d735111397c975f7797d4698604e56415099
dgmodesto <douglasgomesmodesto@gmail.com>
2022-04-27 08:46:07 -0300

    feat(sorted-doubly-linked-list): implementar solução para o problema inserting a Node into a sorted doubly linked list do racker rank

--- ChallengeHackerRank/Program.cs
+++ ChallengeHackerRank/Program.cs
@@ -12,7 +12,7 @@ namespace ChallengeHackerRank
       
         static void Main(string[] args)
         {
-            FindMergeNode.Initial(args);
+            SortedDoublyLinkedList.Initial(args);
 
         }
 
