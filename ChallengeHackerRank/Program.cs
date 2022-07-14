ba7d2d9382a0a4fefbb52f3bed148c227c0a8939
dgmodesto <douglasgomesmodesto@gmail.com>
2022-07-14 13:46:19 -0300

    feat(algorithm): implementar solução para problema do algoexpert.io

--- ChallengeHackerRank/Program.cs
+++ ChallengeHackerRank/Program.cs
@@ -8,15 +8,14 @@ namespace ChallengeHackerRank
       
         static void Main(string[] args)
         {
-            TreeSample.Initial(args);
+            //TreeSample.Initial(args);
 
             //Tree.Tree.Initial(args);
             //Sorting.Sorting.Initial(args);
-<<<<<<< HEAD
-            MinimumDistances.Initial(args);
-=======
+            ValidateSubSequence.Initial(args);
+
             // LengthOfLongestSubstring.Initial(args);
->>>>>>> 433d49f7824df1871220031fad290e1ae92f8c19
+
 
         }
         
---
+++ ChallengeHackerRank/ValidateSubSequence.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChallengeHackerRank
+{
+    public static class ValidateSubSequence
+    {
+        /*
+         https://www.algoexpert.io/questions/validate-subsequence
+         
+          INPUT 
+            array = [5, 1, 22, 25, 6, -1, 8, 10]
+            sequence =  [1, 6, -1, 10 ]
+          OUTPUT
+            true
+
+         */
+
+        public static bool IsValidSubsequence(List<int> array, List<int> sequence)
+        {
+            // Write your code here.
+            foreach (var item in array)
+            {
+                if (sequence.Contains(item))
+                {
+                    var idx = sequence.IndexOf(item);
+                    if (idx != 0) return false;
+                    sequence.RemoveAt(idx);
+                }
+            }
+            return !sequence.Any();
+        }
+
+        public static void Initial(string [] args)
+        {
+            var arr = new List<int> { 5, 1, 22, 25, 6, -1, 8, 10 };
+            var sequence = new List<int> { 1, 6, -1, 10 };
+
+            var res = IsValidSubsequence(arr, sequence);
+
+            Console.WriteLine(res);
+        }
+
+    }
+}
