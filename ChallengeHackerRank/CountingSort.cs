e6e46a35ca2f7b11b485a64b7ae4d95710787ae9
dgmodesto <douglasgomesmodesto@gmail.com>
2022-08-10 20:10:42 -0300

    feat(algorithm): implement solution for problem from hackerhank

---
+++ ChallengeHackerRank/CountingSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChallengeHackerRank
+{
+    public static class CountingSort
+    {
+        /*
+         * https://www.hackerrank.com/challenges/one-week-preparation-kit-countingsort1/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-two&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+         * 
+        
+    * Complete the 'countingSort' function below.
+    *
+    * The function is expected to return an INTEGER_ARRAY.
+    * The function accepts INTEGER_ARRAY arr as parameter.
+    * 
+INPUT 
+100
+63 25 73 1 98 73 56 84 86 57 16 83 8 25 81 56 9 53 98 67 99 12 83 89 80 91 39 86 76 85 74 39 25 90 59 10 94 32 44 3 89 30 27 79 46 96 27 32 18 21 92 69 81 40 40 34 68 78 24 87 42 69 23 41 78 22 6 90 99 89 50 30 20 1 43 3 70 95 33 46 44 9 69 48 33 60 65 16 82 67 61 32 21 79 75 75 13 87 70 33     
+OUTPUT
+0 2 0 2 0 0 1 0 1 2 1 0 1 1 0 0 2 0 1 0 1 2 1 1 1 3 0 2 0 0 2 0 3 3 1 0 0 0 0 2 2 1 1 1 2 0 2 0 1 0 1 0 0 1 0 0 2 1 0 1 1 1 0 1 0 1 0 2 1 3 2 0 0 2 1 2 1 0 2 2 1 2 1 2 1 1 2 2 0 3 2 1 1 0 1 1 1 0 2 2
+
+    */
+
+        public static List<int> countingSort(List<int> arr)
+        {
+            var list = new int [100];
+
+            for (int i = 0; i < arr.Count(); i++)
+            {
+                list[arr[i]]++;
+            }
+
+            return list.ToList();
+
+        }
+
+        public static void Initial(string [] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            List<int> result = countingSort(arr);
+
+            Console.WriteLine(String.Join(" ", result));
+
+        }
+    }
+}
