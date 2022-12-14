23079bf547b6ba1484b96849d085d62fd04b1ab1
dgmodesto <douglasgomesmodesto@gmail.com>
2022-12-14 09:09:29 -0300

    feat(selection-sort): implement solution for the problem from algo expert

---
+++ ChallengeHackerRank/AlgoExpert/SelectionSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChallengeHackerRank.AlgoExpert
+{
+    public static class SelectionSort
+    {
+
+        public static int[] SelectionSort1(int[] array)
+        {
+            // Write your code here.
+
+            int i = 0;
+            int size = array.Length - 1;
+            while (i <= size)
+            {
+                int j = i;
+                while (j <= size)
+                {
+                    if (array[j] < array[i])
+                    {
+                        int swap = array[j];
+                        array[j] = array[i];
+                        array[i] = swap;
+                    }
+                    j++;
+                }
+                i++;
+            }
+
+            return array;
+        }
+
+        public static void Initial(string [] args)
+        {
+            var input = new int[] { 2, 3, 5, 5, 6, 8, 9 };
+
+            var result = SelectionSort1(input);
+
+            Console.WriteLine("Expected: [2, 3, 5, 5, 6, 8, 9]");
+            Console.Write($"Actual: ");
+            Array.ForEach(result, Console.Write);
+        }
+
+    }
+}
