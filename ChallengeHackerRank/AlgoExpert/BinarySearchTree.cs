ad9dcf6b0703e009bf34f0c1efdc03009ee80962
Douglas Modesto <douglas.modesto@prestserv.guide.com.br>
2023-01-23 19:59:22 -0300

    feat(bst-validate): implement solution for the problem from algo expert

--- ChallengeHackerRank/AlgoExpert/BinarySearchTree.cs
+++ ChallengeHackerRank/AlgoExpert/BinarySearchTree.cs
@@ -21,9 +21,9 @@ namespace ChallengeHackerRank.AlgoExpert
             public BST Insert(int value)
             {
 
-                if(this == null) return new BST(value);
+                if (this == null) return new BST(value);
 
-                if(value < this.value)
+                if (value < this.value)
                 {
                     if (this.left != null)
                         this.left = this.left.Insert(value);
@@ -31,9 +31,9 @@ namespace ChallengeHackerRank.AlgoExpert
                         this.left = new BST(value);
                 } else
                 {
-                    if(this.right != null) 
+                    if (this.right != null)
                         this.right = this.right.Insert(value);
-                    else 
+                    else
                         this.right = new BST(value);
                 }
 
@@ -47,7 +47,7 @@ namespace ChallengeHackerRank.AlgoExpert
                 if (value == this.value) return true;
                 else if (value < this.value)
                 {
-                    if(this.left == null) return false;
+                    if (this.left == null) return false;
                     else return this.left.Contains(value);
                 } else
                 {
@@ -59,13 +59,13 @@ namespace ChallengeHackerRank.AlgoExpert
 
             public BST Remove(int value)
             {
-                if (this == null) return this; 
+                if (this == null) return this;
 
-                if(value < this.value)
+                if (value < this.value)
                 {
                     if (this.left != null)
                         this.left = this.left.Remove(value);
-                } else if(value > this.value)
+                } else if (value > this.value)
                 {
                     if (this.right != null)
                         this.right = this.right.Remove(value);
@@ -100,10 +100,12 @@ namespace ChallengeHackerRank.AlgoExpert
                 return ValidateBstHelper(this, int.MinValue, int.MaxValue);
             }
 
+
+
             private bool ValidateBstHelper(BST tree, int minValue, int maxValue)
             {
-                if (tree == null) return true; 
-                if( tree.value < minValue || tree.value >= maxValue) return false;
+                if (tree == null) return true;
+                if (tree.value < minValue || tree.value >= maxValue) return false;
 
                 var leftResult = ValidateBstHelper(tree.left, minValue, tree.value);
                 var rightResult = ValidateBstHelper(tree.right, tree.value, maxValue);
@@ -115,7 +117,7 @@ namespace ChallengeHackerRank.AlgoExpert
             {
                 var current = node;
 
-                while(current.left != null)
+                while (current.left != null)
                 {
                     current = current.left;
                 }
@@ -124,7 +126,130 @@ namespace ChallengeHackerRank.AlgoExpert
             }
         }
 
+        public static class BSTOrders {
+            public static List<int> InOrderTraverseRecursive(BST tree, List<int> array)
+            {
+                // Complexity O(N) | Space O(N) where N is the number of nodes in the BST
+                if (tree == null) return new List<int>();
 
+                var listResult = new List<int>();
+
+                var listLeft = InOrderTraverseRecursive(tree.left, array);
+                var current = tree.value;
+                var listRight = InOrderTraverseRecursive(tree.right, array);
+
+                listResult.AddRange(listLeft);
+                listResult.Add(current);
+                listResult.AddRange(listRight);
+
+                return listResult;
+            }
+            public static List<int> PostOrderTraverseRecursive(BST tree, List<int> array)
+            {
+                // Complexity O(N) | Space O(N) where N is the number of nodes in the BST
+                if (tree == null) return new List<int>();
+
+                var listResult = new List<int>();
+
+                var listLeft = PostOrderTraverseRecursive(tree.left, array);
+                var listRight = PostOrderTraverseRecursive(tree.right, array);
+                var current = tree.value;
+
+                listResult.AddRange(listLeft);
+                listResult.AddRange(listRight);
+                listResult.Add(current);
+
+                return listResult;
+            }
+            public static List<int> PreOrderTraverseRecursive(BST tree, List<int> array)
+            {
+                // Complexity O(N) | Space O(N) where N is the number of nodes in the BST
+                if (tree == null) return new List<int>();
+                var listResult = new List<int>();
+
+                var listLeft = PreOrderTraverseRecursive(tree.left, array);
+                var listRight = PreOrderTraverseRecursive(tree.right, array);
+                var current = tree.value;
+
+                listResult.Add(current);
+                listResult.AddRange(listLeft);
+                listResult.AddRange(listRight);
+
+
+                return listResult;
+            }
+
+
+            public static List<int> InOrderTraverseInterative(BST tree, List<int> array)
+            {
+                // Write your code here.
+
+                var stack = new Stack<BST>();
+                BST curr = null;
+                if (tree != null)
+                    curr = tree;
+
+                while (stack.Count > 0 || curr != null)
+                {
+                    while (curr != null)
+                    {
+                        stack.Push(curr);
+                        curr = curr.left;
+                    }
+
+                    curr = stack.Pop();
+                    array.Add(curr.value);
+                    curr = curr.right;
+                }
+
+                return array;
+            }
+            public static List<int> PreOrderTraverseInterative(BST tree, List<int> array)
+            {
+                // Write your code here.
+                var stack = new Stack<BST>();
+                if (tree != null)
+                    stack.Push(tree);
+
+                while (stack.Count > 0)
+                {
+
+                    var curr = stack.Pop();
+                    array.Add(curr.value);
+                    if (curr.right != null)
+                        stack.Push(curr.right);
+                    if (curr.left != null)
+                        stack.Push(curr.left);
+                }
+
+
+                return array;
+            }
+            public static List<int> PostOrderTraverseInterative(BST tree, List<int> array)
+            {
+                // Write your code here.
+                var stack = new Stack<BST>();
+                if (tree != null)
+                    stack.Push(tree);
+
+                while (stack.Count > 0)
+                {
+                    var curr = stack.Pop();
+                    array.Add(curr.value);
+                    if (curr.left != null)
+                    {
+                        stack.Push(curr.left);
+                    }
+                    if (curr.right != null)
+                    {
+                        stack.Push(curr.right);
+                    }
+                }
+
+                array.Reverse();
+                return array;
+            }
+        }
 
 
         public static void Initial(string [] args)
@@ -140,6 +265,26 @@ namespace ChallengeHackerRank.AlgoExpert
             root.right.right = new BST(22);
 
 
+            Console.WriteLine($"InOrderTraverse");
+            Console.WriteLine($"Expected: 12551013141522");
+            Console.Write($"Result  : ");
+            Array.ForEach(BSTOrders.InOrderTraverseRecursive(root, new List<int>()).ToArray(), Console.Write);
+            Console.WriteLine();
+            Console.WriteLine($"-------------------------------------------");
+
+            Console.WriteLine($"PostOrderTraverse");
+            Console.WriteLine($"Expected: 12551413221510");
+            Console.Write($"Result  : ");
+            Array.ForEach(BSTOrders.PostOrderTraverseRecursive(root, new List<int>()).ToArray(), Console.Write);
+            Console.WriteLine();
+            Console.WriteLine($"-------------------------------------------");
+
+            Console.WriteLine($"PreOrderTraverse");
+            Console.WriteLine($"Expected: 10521515131422");
+            Console.Write($"Result  : ");
+            Array.ForEach(BSTOrders.PreOrderTraverseRecursive(root, new List<int>()).ToArray(), Console.Write);
+            Console.WriteLine();
+            Console.WriteLine($"-------------------------------------------");
 
 
             root.Insert(12);
@@ -164,6 +309,8 @@ namespace ChallengeHackerRank.AlgoExpert
             Console.WriteLine($"Expected: True");
             Console.WriteLine($"Result: { root.ValidateBst()}");
             Console.WriteLine($"-------------------------------------------");
+
+
 
 
         }
