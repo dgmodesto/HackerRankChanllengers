2a04f072678112c6d38c044666d25fefc6f1fc21
dgmodesto <douglasgomesmodesto@gmail.com>
2024-03-27 17:27:38 -0300

    feat(LongestCommonPrefix): implement solution for the problem from leetcode

---
+++ ChallengeHackerRank/LeetCode/LongestCommonPrefix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ChallengeHackerRank.LeetCode
+{
+    public static class LongestCommonPrefix
+    {
+        public static string LongestCommonPref(string[] ss)
+        {
+            StringBuilder sb = new StringBuilder();
+            string shortest = ss.OrderBy(s => s.Length).First();
+
+            foreach(var(c,i) in shortest.Select((c, i) => (c, i)))
+            {
+                if (ss.Any(s => s[i] != c)) break;
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+
+        public static void Initial(string[] args)
+        {
+            string[] input = new string[] { "flower", "flow", "flight" };
+            string output = "fl";
+
+            var result = LongestCommonPref(input);
+
+            Console.WriteLine("Expected : {0} ", output);
+            Console.WriteLine("Result :{0} ", result);
+        }
+    }
+}
